c50877eac0ab06ed1f7b797d3057490e4169a8ef
VladProgrammer08 <bumbuv136@gmail.com>
2024-01-06 18:14:35 -0800

    Added text and button elements to the form

--- GettingStarted/Form1.Designer.cs
+++ GettingStarted/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnSubmit = new Button();
+            txtFirstName = new TextBox();
+            txtLastName = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(36, 271);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(94, 29);
+            btnSubmit.TabIndex = 0;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(134, 70);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(125, 27);
+            txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(134, 150);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(125, 27);
+            txtLastName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 20);
+            label1.TabIndex = 3;
+            label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 157);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Last Name";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(440, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtLastName);
+            Controls.Add(txtFirstName);
+            Controls.Add(btnSubmit);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnSubmit;
+        private TextBox txtFirstName;
+        private TextBox txtLastName;
+        private Label label1;
+        private Label label2;
     }
 }
